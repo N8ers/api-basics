49a07957e12c62e057a14b453c80b45f26739e95
N8ers <nathanraysheryak@gmail.com>
2018-09-10 12:04:46 -0400

    lunch

---
+++ src/Apicall.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Apicall extends Component {
+    render() {
+        return(
+            <div>
+                <h3>I am from Apicall</h3>
+            </div>
+        );
+    }
+}
+
+export default Apicall;
